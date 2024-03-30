d62a61b2264be601797aea231569464b1211e900
Luiz Henrique Araújo <araujoluiz.dev@gmail.com>
2024-03-30 11:34:33 -0300

    First Commit

---
+++ MauiShellAutoRoutesSample/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiShellAutoRoutesSample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Shell.Current.GoToAsync(Routes.SomePage);
+        }
+    }
+
+}
---
+++ MauiShellAutoRoutesSample/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using epj.RouteGenerator;
+
+namespace MauiShellAutoRoutesSample
+{
+    //Add this sufix to search all Page in your app, like SomePage, MainPage, and create the route for them.
+    [AutoRoutes("Page")]
+    //If you want to create a especific route, you can add a ExtraRoute, with the page name.
+    [ExtraRoute("Dashboard")]
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
